143888924472a8cc4202161c6b7cb124411b9714
Suraj Rai <77954411+devsurajrai@users.noreply.github.com>
2021-07-30 09:28:49 +0530

    chore: react app changes

--- Frontend/react/devsnest-react-thas/src/App.js
+++ Frontend/react/devsnest-react-thas/src/App.js
@@ -1,11 +1,39 @@
+import './App.css'
 
+var boxArr=[]
 
+let firstBlack=  <div className="box-container">
+                      <span className="dark"></span>
+                      <span className="light"></span>
+                  </div>
+
+let firstWhite= <div className="box-container">
+                      <span className="light"></span>
+                      <span className="dark"></span>
+                </div>
+
+function chessBoxesArr(){
+        for(let i=1;i<=8;i++){
+            for(let j=1;j<=4;j++){
+                  if(i%2===0){
+                     boxArr.push(firstWhite)
+                  }
+                  else{
+                    boxArr.push(firstBlack)
+                  }
+            }
+        }
+}
+chessBoxesArr()
+var chessBoxes=boxArr.map(item =>{return(item)})
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
+  return(
+    <div className="main-container">
+        <div className="board">
+            {chessBoxes}
+        </div>
     </div>
-  );
+)
 }
 
 export default App;
--- Frontend/react/devsnest-react-thas/src/index.js
+++ Frontend/react/devsnest-react-thas/src/index.js
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 //THA Imports -->
-// import App from './App';
+import App from './App';
 // import DayFifteen from './THA/day-15/dayFifteen'
 // import DaySixteenA from './THA/day-16/day-16-a'
-import DaySixteenB from './THA/day-16/day-16-b'
+// import DaySixteenB from './THA/day-16/day-16-b'
 
 //THA Imports Ends ---->
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <DayFifteen /> */}
     {/* <DaySixteenA /> */}
-    <DaySixteenB />
+    {/* <DaySixteenB /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
